4c7e33f6b7fbf36dbaddd9bb0ef31569532e2ac1
Lotaku <qq317399510@gmail.com>
2014-10-08 16:01:34 +0800

    resolve CSRF error ,but has 500 error

--- django_learn/a1/views.py
+++ django_learn/a1/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, HttpResponse ,render_to_response
 from django.template import RequestContext
+import json
+#from django.utils import
 # Create your views here.
 
 def a1_index(request):
-    context = {}
+    if request.method == 'POST' and request.is_ajax():
+        name = 'fang'
+        city = 'ST'
+        message = name + ' lives in ' + city
+        print 'xxx'
 
-    return render_to_response('a1/a1.html',context,
-                context_instance=RequestContext(request),
-            )
+        return render_to_response('a1/a1.html',json.dumps({'message': message}),\
+context_instance=RequestContext(request))
+    return render(request, 'a1/a1.html')
