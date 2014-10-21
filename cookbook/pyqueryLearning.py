b6e188fc00baffaad78fefcf55b2298333ea9ad9
Lotaku <qq317399510@gmail.com>
2014-10-21 18:11:19 +0800

    采集学习

--- cookbook/pyqueryLearning.py
+++ cookbook/pyqueryLearning.py
@@ -1,25 +1,15 @@
 #- * - conding:utf-8 - * -
+import requests
 from pyquery import PyQuery as pq
-#d = pq('<p class="hello" id="hello">you know Python rocks</p>')
-#p = d('p')
-#print p
-#p.prepend('check out <a href="http://reddit.com/r/python">reddit</a>')
-#print p.html()
 
-#a = pq('<html><body><div id="test"><a href="http://python.org">python</a> !</div></body></html>')
-#print a('#test').html()
-#p.prependTo(a('#test'))
-#print a('#test').html()
-#print p.html()
-#p.insertAfter(a('#test'))
-#print a('#test').html()
-#print a
-#print a
-#a.remove('#test')
-#d =pq('<p class="hello">Hi</p><p>Bye</p><div></div>')
-#print d('p').eq(0).attr('class')
-h = pq(url='http://filmav.com/')
-p = h(':contains("post")')
-#p = h('div#post-51114')
-print p('h2').html()
-
+h = requests.get(url='http://www.iplaysoft.com')
+#print h.headers['content-type']
+#print h.encoding
+#print h.json()
+a = pq(h.text)
+# print a('div').find('.entry-title').eq(1).find('a').html()
+# data = a('div').find('.entry-title')
+data = a('h2.entry-title')
+for d in data:
+    # print dir(d)
+    print pq(d)('a').attr('href')
