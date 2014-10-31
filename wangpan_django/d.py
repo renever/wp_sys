f70ef032af121f2a11774d93fddbe5663257702f
Lotaku <qq317399510@gmail.com>
2014-10-31 18:10:27 +0800

    测试中

--- wangpan_django/d.py
+++ wangpan_django/d.py
@@ -1,12 +1,15 @@
 # - * - coding:utf-8 - * -
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait  # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC  # available since 2.26.0
 
 url_login = 'http://www.uploadable.ch/login.php'
 # url_download = 'http://pdl.uploadable.ch/file/SwkwFPd7scRC/yJjjJPhBdYlJIVTFXbCBgG0K77t5yexeS1SMccAZUPpTkUjjVCx4Yf86FyYTYDsKhdKryZC_e4_h9Ezc-ZfP0RQroH5C4v3gAyHOaMr85vSTHxjZQY9g-XdCY6FlWSTR12fS_S96MNRKjicseawPW69Xfge3QYdlULaAPv6yc9Y./123.pdf'
 url_download = 'http://www.uploadable.ch/file/SwkwFPd7scRC/123.pdf'
+
+ffprofile = webdriver.FirefoxProfile("/home/lotaku/.mozilla/firefox/mwad0hks.default")
 
 # Create a new instance of the Firefox driver
 driver = webdriver.Firefox()
@@ -20,7 +23,8 @@ print driver.title
 # find the element that's name attribute is q (the google search box)
 inputElement_userName = driver.find_element_by_name("userName")
 inputElement_userPassword = driver.find_element_by_name("userPassword")
-
+inputElement_userName.clear()
+inputElement_userPassword.clear()
 # type in the search
 inputElement_userName.send_keys('lxl001')
 inputElement_userPassword.send_keys('qQ2@wW')
@@ -29,11 +33,13 @@ btn_login = driver.find_element_by_id("loginFormSubmit")
 btn_login.click()
 
 
-# try:
-
-# WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located())
-a = driver.get(url_download)
-print a
+try:
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "dashboard_button")))
+    driver.get(url_download)
+    a = driver.switch_to_alert()
+    a.accept()
+except Exception,e:
+    print 'start down...'
 
 
 print "...."
